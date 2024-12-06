6761838da9b0279a871a9354ea7461dcc680af75
KellieB94 <sba24284@student.cct.ie>
2024-12-06 18:43:03 +0000

    Cleaning up comments on all TASKS so far
    adding additional comments and cleaning up the display

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -35,13 +35,15 @@ public class CA2 {
         //the for loop enables the user to input more than one element 
         //System.out.println("Enter the array elements: ");
         //for(int a = 0; a < size; a++){
+        
+            //variable to store the user input 
             //ray[a] = input.nextInt();
         //}
         
         //boolean to find the first-repeated element in the array 
         //boolean Found = false;
         
-        //Array Traversal using two nested for-loops, one running through the outer-loop and one running through the inner-loop,
+        //Array Traversal using two nested for-loops, one runs through the outer-loop and one runs through the inner-loop,
         //to scan through each array element to find the first-repeated element in the array
         //for(int a = 0; a < size; a++){
             //for(int b = a + 1; b < size; b++){
@@ -55,15 +57,18 @@ public class CA2 {
                     //break; 
                 //}
             //}
+            
             //breaking the loop if the first-repeated element is found
             //if(Found){
                 //break;
             //}
         //}
+        
         //print-statement if it occurs that no repeated-element is found
         //if(!Found) {
             //System.out.println("No repeat-elements found!");
         //}
+        
         //closing the Scanner class to prevent errors and stop more user input 
         //input.close();
         
@@ -83,12 +88,15 @@ public class CA2 {
         //int[][] mulTable = new int[a][a];
        
         //populating the multiplication table by adding values, using two nested for-loops, 
-        //one going through the outer-loop and one going through the inner-loop to give the multiplication products 
+        //one runs through the outer-loop and one runs through the inner-loop to give the multiplication products 
         //for(int b = 0; b < a; b++){
             //for(int c = 0; c < a; c++){
+            
+                //variable to store the multiplication products
                 //mulTable[b][c] = (b + 1) * (c + 1);
             //}
         //}
+        
         //displaying the multiplication table in the form of a matrix,
         //using two nested for-loops, one runs through the outer-loop and the other runs through the inner-loop,
         //to ensure a neat format 
@@ -96,12 +104,15 @@ public class CA2 {
         //for(int b = 0; b < a; b++){
             //for(int c = 0; c < a; c++){
             
-                //printing each element in a size 3 alignment to make the table easier to read 
+                //printing each element in a nice-sized (5) alignment to make the table easier to read 
                 //System.out.printf("%5d", mulTable[b][c]);
             //}
+            
             //function to print a new line after every row, for easier reading 
             //System.out.println();
+            
         //}
+        
         //closing the Scanner class to stop user input capability and to prevent any errors  
         //input.close();
         
@@ -109,60 +120,61 @@ public class CA2 {
         //TASK 3 - calculating the sum of the main-diagonal and the secondary-diagonal of a square matrix 
         
         //calling the scanner class by creating object "input" to take user input 
-        Scanner input = new Scanner(System.in);
+        //Scanner input = new Scanner(System.in);
         
         //prompting input from the user to give number of rows in the matrix 
-        System.out.println("Enter the number of rows: ");
+        //System.out.println("Enter the number of rows: ");
         
         //variable to store the number of rows 
-        int row = input.nextInt();
+        //int row = input.nextInt();
         
         //prompting input from the user to give number of columns in the matrix 
-        System.out.println("Enter the number of columns: ");
+        //System.out.println("Enter the number of columns: ");
         
         //variable to store the number of columns 
-        int cols = input.nextInt();
+        //int cols = input.nextInt();
         
         //initializing the matrix 
-        int[][] square = new int [row][cols];
+        //int[][] square = new int [row][cols];
         
         //prompting the user to enter row by row elements 
-        System.out.println("Enter elements: ");
+        //System.out.println("Enter elements: ");
         
         //initialization of two nested for-loops to allow user to enter elements one by one,
         //one runs through the outer-loop and the other runs through the inner-loop
-        for(int a = 0; a < row; a++){
-            for(int b = 0; b < cols; b++){
+        //for(int a = 0; a < row; a++){
+            //for(int b = 0; b < cols; b++){
                 
                 //variable to store the elements inputted by user 
-                square[a][b] = input.nextInt();
-            }
-        }
+                //square[a][b] = input.nextInt();
+            //}
+        //}
         
         //display of the matrix in 2D form 
         //initialization of two nested for-loops to display the matrix table 
-        for(int a = 0; a < row; a++){
-            for(int b = 0; b < cols; b++){
+        //for(int a = 0; a < row; a++){
+            //for(int b = 0; b < cols; b++){
                 
                 //print-statement to show the table in a readable size for blind people like me ;)
-                System.out.printf("%5d", square[a][b]);
-            }
+                //System.out.printf("%5d", square[a][b]);
+            //}
             
             //function to print a new line every row, for easier reading 
-         System.out.println("");
-    }
+         //System.out.println("");
+     //}
          //variables to store the sum of diagonals 
-         int primarySum = 0;
-         int secondarySum = 0;
+         //int primarySum = 0;
+         //int secondarySum = 0;
     
-         //diagonal-sum calculation 
-         for(int a = 0; a < row; a++){
-             primarySum += square[a][a];
-             secondarySum += square[a][cols - a - 1];
-         }
-         //output statements  
-         System.out.println("Main diagonal sum: " + primarySum);
-         System.out.println("Secondary diagonal sum: " + secondarySum);
+         //diagonal-sum calculation using a for-loop 
+         //for(int a = 0; a < row; a++){
+             //primarySum += square[a][a];
+             //secondarySum += square[a][cols - a - 1];
+         //}
+         
+         //print-statement to output the main-diagonal and secondary diagonal-sum to the user 
+         //System.out.println("Main diagonal sum: " + primarySum);
+         //System.out.println("Secondary diagonal sum: " + secondarySum);
 }
 }
     
