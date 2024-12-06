58a8149903180339faeb40daabb82ddf7cf1d650
KellieB94 <sba24284@student.cct.ie>
2024-12-06 20:46:30 +0000

    5 - if-statement to swap and compare
    statement to swap and compare elements if they are in the wrong order, variable initialization for swapping the elements

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -277,29 +277,30 @@ public class CA2 {
            int b = 0;
            
            ////two nested-loops to run through the array elements, the outer-loop is for the number of passes,
-           ////
+           ////the inner-loop is adjacent element comparison 
         for (int i = 0; i < a - 1; i++) {
-            // Inner loop for comparing adjacent elements
+            ////Inner loop for comparing adjacent elements
             for (int j = 0; j < a - 1 - i; j++) {
-                // Compare and swap if elements are in wrong order
+                
+                ////if-statement to swap and compare elements if they're in the wrong order
                 if (ray[j] > ray[j + 1]) {
-                    // Swap the elements
+                    
+                    ////variable for swapping the elements 
                     int swap = ray[j];
                     ray[j] = ray[j + 1];
                     ray[j + 1] = swap;
-                    b++; // Increment the swap count
+                    b++; 
                 }
             }
         }
 
-        // Display total number of swaps
+        //displaying the total number of swaps to the user 
         System.out.println("Total number of swaps: " + b);
     }
            
            
-
-    
-         //public static void main(String[] args) {
+           
+        public static void main(String[] args) {
              
            
              
