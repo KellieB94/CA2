3881ced170539e081da5b71d5594b4fde6a5cc07
KellieB94 <sba24284@student.cct.ie>
2024-12-06 16:31:28 +0000

    Added Scanner to TASK 2
    Successfully called the scanner class to input from the user the size of the multiplication table

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -15,55 +15,73 @@ public class CA2 {
     //GitHub Repo:
     //https://github.com/KellieB94/CA2.git
     
-    //function to find the first-repeated element in an array of integers
     public static void main(String[] args) {
-        
-        //initializing the Scanner Class with an object "input" to use the Scanner Class function 
-        Scanner input = new Scanner(System.in);
+    
+        //TASK 1 - function to find the first-repeated element in an array of integers 
+    
+        //initializing the Scanner Class to take user input with an object "input" to use the Scanner Class function 
+        //Scanner input = new Scanner(System.in);
         
         //prompting the user to give array size as input
-        System.out.print("Enter the array size: ");
+        //System.out.print("Enter the array size: ");
         
         //variable to store the given array size 
-        int size = input.nextInt();
+        //int size = input.nextInt();
         
         //initializing the array 
-        int[] ray = new int [size];
+        //int[] ray = new int [size];
         
         //prompting the user to give array elements as input using for loop 
         //the for loop enables the user to input more than one element 
-        System.out.println("Enter the array elements: ");
-        for(int a = 0; a < size; a++){
-            ray[a] = input.nextInt();
-        }
+        //System.out.println("Enter the array elements: ");
+        //for(int a = 0; a < size; a++){
+            //ray[a] = input.nextInt();
+        //}
         
         //boolean to find the first-repeated element in the array 
-        boolean Found = false;
+        //boolean Found = false;
         
         //Array Traversal using two nested for-loops to scan through each array element to find the first-repeated element in the array
-        for(int a = 0; a < size; a++){
-            for(int b = a + 1; b < size; b++){
+        //for(int a = 0; a < size; a++){
+            //for(int b = a + 1; b < size; b++){
                 
                 //conditional if-statement to print to the user the first-repeated element
-                if(ray[a] == ray[b]){
-                    System.out.println("The first repeated element is: " + ray[a]);
+                //if(ray[a] == ray[b]){
+                    //System.out.println("The first repeated element is: " + ray[a]);
                     
                     //breaking the loop on the condition that first-repeated element is found 
-                    Found = true;
-                    break; 
-                }
-            }
+                    //Found = true;
+                    //break; 
+                //}
+            //}
             //breaking the loop if the first-repeated element is found
-            if(Found){
-                break;
-            }
-        }
+            //if(Found){
+                //break;
+            //}
+        //}
         //print-statement if it occurs that no repeated-element is found
-        if(!Found) {
-            System.out.println("No repeat-elements found!");
-        }
+        //if(!Found) {
+            //System.out.println("No repeat-elements found!");
+        //}
         //closing the Scanner class to prevent errors and stop more user input 
-        input.close();
+        //input.close();
+        
+        //TASK 2 - function that generates a multiplication table using a 2D array 
+        
+        //calling the Scanner class to take user input by initializing the Scanner object "input"
+        Scanner input = new Scanner(System.in);
+        
+        //prompting the user to enter the multiplication table size 
+        System.out.println("Enter the number of rows and columns in table (Example: 5x5): ");
+        
+        //variable to store the table size 
+        int a = input.nextInt();
+        
+        //initializing 2D array to store the values of the multiplication 
+        int[][] mulTable = new int[a][a];
+       
+        //populating the multiplication table by adding values 
+       
     }
     
 }
