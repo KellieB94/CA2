a5d410d6848ba05d3bd1503b401899586b2022a0
KellieB94 <sba24284@student.cct.ie>
2024-12-06 15:23:22 +0000

    Added Scanner Class to Task 1
    Successfully initialized Scanner to input user data to print array size and elements, using a for loop to print the elements

--- src/ca2/CA2.java
+++ src/ca2/CA2.java
@@ -11,14 +11,30 @@ package ca2;
 //importing the Scanner Class to add Scanner object 
 import java.util.Scanner;
 public class CA2 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    //GitHub Repo:
+    //https://github.com/KellieB94/CA2.git
+    
+    //function to find the first-repeated element in an array of integers
     public static void main(String[] args) {
-        // TODO code application logic here
         //initializing the Scanner Class with an object "input" to use the Scanner Class function 
         Scanner input = new Scanner(System.in);
+        
+        //prompting the user to give array size as input
+        System.out.print("Enter The Array Size: ");
+        //variable to store the given array size 
+        int size = input.nextInt();
+        
+        //initializing the array 
+        int[] ray = new int [size];
+        
+        //prompting the user to give array elements as input using for loop 
+        //the for loop enables the user to input more than one element 
+        System.out.println("Enter The Array Elements: ");
+        for(int a = 0; a < size; a++){
+            ray[a] = input.nextInt();
+        }
+        
         
     }
     
